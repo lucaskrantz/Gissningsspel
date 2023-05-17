8206c9ad051b2de83916f967af34cda3dd608ede
Lucas Krantz <lucas.krantz@elev.ga.ntig.se>
2023-05-17 13:56:01 +0200

    Completed

---
+++ Gissningsspel/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+// GISSNINGSSPEL
+// GISSA ETT TAL
+// IFALL TALET ÄR DET SAMMA, "GRATTIS"
+// ANNARS "GISSA IGEN"
+// ANVÄND TRYPARSE FÖR ATT FÅNGA GISSNINGAR SOM INTE ÄR SIFFOR
+
+Random generator = new Random();
+bool playing = true;
+int svar = generator.Next(1, 11);
+int g;
+
+Console.WriteLine("Gissa på ett tal mellan 1 - 10");
+
+while (playing)
+{
+    string gissning = Console.ReadLine();
+    bool isParsable = Int32.TryParse(gissning, out g);
+
+    if (!isParsable || g > 10 || g < 1)
+    {
+        Console.WriteLine("Fel! Skriv en siffra mellan 1 - 10");
+    }
+
+    if (g == svar)
+    {
+        Console.WriteLine("Grattis");
+        playing = false;
+    }
+    else
+    {
+        Console.WriteLine("Försök igen");
+
+    }
+
+}
+
+if (!playing)
+{
+    Console.WriteLine("Du vann!");
+}
+
+Console.ReadLine();
+
+
